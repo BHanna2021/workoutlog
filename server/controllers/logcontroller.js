b35a1b0215c69bbc1c90e05b142e56bdfde60ff1
Hanna <Bethany.Hanna@autofinance.com>
2021-06-21 20:44:26 -0400

    completed server side work and added screenshots of testing for readme

--- server/controllers/logcontroller.js
+++ server/controllers/logcontroller.js
@@ -1,12 +1,97 @@
-/* need /log/ POST to create logs, /log/ GET to search all logs for user
-    /log/:id GET to get individual logs for a user, PUT to update an individual log for a user, DELETE to remove an individual log for a user
-*/
-
 const router = require("express").Router();
+const { JsonWebTokenError } = require("jsonwebtoken");
+const validateJWT = require("../../../ElevenJournal/server/middleware/validate-jwt");
 const { LogModel } = require("../models");
+const Log = require("../models/log");
 
-router.get('/test', (req, res) => {
-    res.send('Hi! You have successfully tested.')
+router.post("/", validateJWT, async(req, res) => {
+    const { description, definition, result } = req.body.Log;
+    const { id } = req.user;
+    const logEntry = {
+        description,
+        definition,
+        result,
+        owner_id: id
+    }
+    try {
+        const newLog = await LogModel.create(logEntry);
+        res.status(200).json(newLog);
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
 });
 
+router.get("/", validateJWT, async(req, res) => {
+    const { id } = req.user;
+    try {
+        const entries = await LogModel.findAll({
+            where: {
+                owner_id: id
+            }
+        })
+        res.status(200).json(entries);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+router.get("/:id", validateJWT, async(req, res) => {
+    const logId = req.params.id;
+    const ownerId = req.user.id;
+    try {
+        const results = await LogModel.findAll({
+            where: {
+                owner_id: ownerId,
+                id: logId
+            }
+        });
+        res.status(200).json(results);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+router.put("/:id", validateJWT, async(req, res) => {
+    const { description, definition, result } = req.body.Log;
+    const ownerId = req.user.id;
+    const logId = req.params.id;
+
+    const query = {
+        where: {
+            id: logId,
+            owner_id: ownerId
+        }
+    };
+    const updatedLog = {
+        description: description,
+        definition: definition,
+        result: result
+    };
+    try {
+        const update = await LogModel.update(updatedLog, query);
+        res.status(200).json({
+            message: `Log ${logId} has been updated.`
+        });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+router.delete("/:id", validateJWT, async(req, res) => {
+    const ownerId = req.user.id;
+    const logId = req.params.id;
+
+    try {
+        const query = {
+            where: {
+                id: logId,
+                owner_id: ownerId
+            }
+        };
+        await LogModel.destroy(query);
+        res.status(200).json({ message: `Log ${logId} has been removed.` });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
 module.exports = router;
